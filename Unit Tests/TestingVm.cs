0f3c40715af72e14e4a7458878a465c5aa87fc01
moswald <moswald@gmail.com>
2015-06-19 21:01:59 -0500

    adding project

---
+++ Unit Tests/TestingVm.cs
@@ -0,0 +1,29 @@
+﻿namespace Unit_Tests
+{
+    using System;
+    using Microsoft.Reactive.Testing;
+    using ViewModels;
+    using Xunit;
+
+    public class TestingVM
+    {
+        [Fact]
+        public void SomeCountIncreasesToTenByOne()
+        {
+            var sched = new TestScheduler();
+
+            var vm = new FooViewModel(sched);
+
+            var expected = 0L;
+            vm.SomeCount.Subscribe(val => Assert.Equal(expected, val));
+
+            for (var i = 0; i != 10; ++i)
+            {
+                expected = i;
+                sched.AdvanceBy(TimeSpan.FromSeconds(1).Ticks);
+            }
+
+            sched.AdvanceBy(TimeSpan.FromSeconds(1).Ticks);
+        }
+    }
+}
---
+++ ViewModels/FooViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ViewModels
+{
+    using System;
+    using System.Reactive.Concurrency;
+    using System.Reactive.Linq;
+    using ReactiveUI;
+
+    public class FooViewModel : ReactiveObject
+    {
+        public IObservable<long> SomeCount { get; set; } 
+
+        public FooViewModel(IScheduler sched)
+        {
+            SomeCount = Observable.Interval(TimeSpan.FromSeconds(1), sched).Take(10);
+        }
+    }
+}
